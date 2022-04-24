c974009b78c989fffc06846d4ec572eb7e62848e
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-04-25 03:44:13 +0700

    Add files via upload

---
+++ ipsec/add-pptp.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${CYAN}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+echo "Checking VPS"
+clear
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+PUBLIC_IP=$(wget -qO- icanhazip.com);
+else
+PUBLIC_IP=$IP
+fi
+until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Username: " -e VPN_USER
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/premium-script/data-user-pptp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+read -p "Password: " VPN_PASSWORD
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%d-%m-%Y"`
+created=`date -d "0 days" +"%d-%m-%Y"`
+clear
+
+# Add or update VPN user
+cat >> /etc/ppp/chap-secrets <<EOF
+"$VPN_USER" pptpd "$VPN_PASSWORD" *
+EOF
+
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets*
+echo -e "### $VPN_USER $exp">>"/var/lib/premium-script/data-user-pptp"
+cat <<EOF
+
+—————————————————
+       🌟 **PPTP VPN ACCOUNT** 🌟
+—————————————————
+Server IP  : $PUBLIC_IP
+Username   : $VPN_USER
+Password   : $VPN_PASSWORD
+Expired    : $exp
+—————————————————
+EOF
